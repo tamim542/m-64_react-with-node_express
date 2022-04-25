c7fd18561662821851424f8edeaf9eee385f9746
unknown <mdtauhidulislam509@gmail.com>
2022-04-25 14:18:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+  //   fetch('http://localhost:5000/user',{method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify(user)
+  // })
+  // .then(response => response.json())
+  // .then(data => setUsers(data));
+  },[])
+
+  const handleForm=(event)=>{
+    event.preventDefault();
+    const nam=event.target.name.value;
+    const email=event.target.email.value;
+    const user={nam,email};
+    fetch('http://localhost:5000/user',{method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user)
+  })
+  .then(response => response.json())
+  .then(data => setUsers(data));
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>hellow</h1>
+      <form onSubmit={handleForm}>
+        <input type="text" name='name' placeholder='name'/><br></br>
+        <input type="email" name='email' placeholder='email'/><br></br>
+        <button>abc</button>
+      </form>
     </div>
   );
 }
